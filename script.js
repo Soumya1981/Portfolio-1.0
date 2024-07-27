a75e8b3a3e979a68acf3b0803ebe7653a018b50a
Soumyadip Manna <soumyadip1981m@gmail.com>
2024-07-27 13:12:27 +0530

    my project creatred

---
+++ script.js
@@ -0,0 +1,10 @@
+// < !--Setup and start animation! -- >
+var typed = new Typed('#element', {
+                strings: ['Web Developer.', 'Programmer.', 'Video Editor.', 'Stock Market Trader.', 'Gamer.'],
+                typeSpeed: 50,
+});
+            
+var typed = new Typed('#span2', {
+                strings: ['Watching Movies, Playing Cricket, Gardening.'],
+                typeSpeed: 50,
+            });
